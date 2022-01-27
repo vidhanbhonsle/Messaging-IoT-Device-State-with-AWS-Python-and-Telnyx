8533f178225f9da960823dcd34285e45c6c8d75e
Vidhan Bhonsle <vidhanbhonsle@gmail.com>
2022-01-27 22:30:36 +0530

    Update publishData.py

--- publishData.py
+++ publishData.py
@@ -19,13 +19,13 @@ mqttc = paho.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
-awshost = "AWS_END_POINT"
+awshost = "AWS_END_POINT" # Acquire from AWS IoT > Settings
 awsport = 8883
-clientId = "CLIENT_ID"
-thingName = "THING_NAME"
-caPath = "root-CA.crt"
-certPath = "THING_NAME.cert.pem"
-keyPath = "THING_NAME.private.key"
+clientId = "CLIENT_ID" # The unique ID of your choice
+thingName = "THING_NAME" # The name you set for IoT Thing
+caPath = "root-CA.crt" # From connection kit
+certPath = "THING_NAME.cert.pem" # From connection kit
+keyPath = "THING_NAME.private.key" # From connection kit
 
 mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 
@@ -33,6 +33,7 @@ mqttc.connect(awshost, awsport, keepalive=60)
 
 mqttc.loop_start()
 
+# The code block is a infinite loop generating temperature value between 20 and 25. You can replace dummy data with your own sensor value
 while 1==1:
     sleep(10)
     if connflag == True:
