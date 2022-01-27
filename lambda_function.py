f6e13fc37cbd7eaebbcb9db7187a76c627e317f7
Vidhan Bhonsle <vidhanbhonsle@gmail.com>
2022-01-27 22:07:08 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,12 +6,12 @@ def lambda_handler(event, context):
     #format data from message text
     temp_data = float(event['temperature'])
     
-    if temp_data > 22.0:
+    if temp_data > 24.0: # You can change the value as per your use case
       conn = http.client.HTTPSConnection("api.telnyx.com")
       payload = json.dumps({
-      "from": "TELNYX_PHONE_NUMBER",
+      "from": "TELNYX_PHONE_NUMBER", # Obtain number from portal.telnyx.com
       "to": "YOUR_PHONE_NUMBER",
-      "text": "The temperature is "+ str(temp_data) + " degree celsius"
+      "text": "The temperature is "+ str(temp_data) + " degree celsius" # A text you will receive on YOUR_PHONE_NUMBER
       })
       headers = {
       'Content-Type': 'application/json',
